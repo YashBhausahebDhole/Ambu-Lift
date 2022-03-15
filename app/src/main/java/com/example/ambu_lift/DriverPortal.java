cbc099b44c821cba0df606fdc797c5f6640f61f4
yashlearner07 <yasuipor@gmail.com>
2022-03-15 20:30:03 +0530

    changes done Driver main portal

---
+++ app/src/main/java/com/example/ambu_lift/DriverPortal.java
@@ -0,0 +1,14 @@
+package com.example.ambu_lift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverPortal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_portal);
+    }
+}
