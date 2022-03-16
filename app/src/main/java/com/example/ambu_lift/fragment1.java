eecaddb29c87fde0c87361bc3580c1ec6c42f832
AtharvaM2003 <atharva2mundhe@gmail.com>
2022-03-17 02:01:10 +0530

    Driver Registration done

--- app/src/main/java/com/example/ambu_lift/fragment1.java
+++ app/src/main/java/com/example/ambu_lift/fragment1.java
@@ -1,10 +1,12 @@
 package com.example.ambu_lift;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 public class fragment1 extends Fragment {
 
     Spinner spinner;
+    Button locbtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,9 +22,19 @@ public class fragment1 extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_fragment1, container, false);
         spinner = v.findViewById(R.id.ambuspin);
+        locbtn=v.findViewById(R.id.locbtn);
+        locbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),emerlocation.class);
+                startActivity(intent);
+            }
+        });
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
         myAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(myAdapter);
         return v;
+
+
     }
 }
